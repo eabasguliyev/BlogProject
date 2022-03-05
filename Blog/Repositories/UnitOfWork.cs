b0a337a9520f16544a30c3da0197a443a8100fb3
Abbasguliyev Elgun Fiday <Abba_9lhw@itstep.edu.az>
2022-03-05 15:28:37 +0400

    add repository and unitofwork

---
+++ Blog/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Repositories.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork()
+        {
+
+        }
+        public void Save()
+        {
+            
+        }
+    }
+}
